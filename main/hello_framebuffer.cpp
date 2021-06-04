99d39d5930d51b2dd590d6931845057e319399d0
Oleg <loshkin.oleg.95@gmail.com>
2021-06-04 12:59:53 +0200

    Oleg: hello_framebuffer is now working.

--- main/hello_framebuffer.cpp
+++ main/hello_framebuffer.cpp
@@ -18,7 +18,10 @@ public:
 protected:
     Camera camera_;
     Model crate_;
-    Quad quad_;
+
+    unsigned int quadVAO_ = 0, quadVBO_ = 0;
+    Shader framebufferShader_ = Shader();
+    unsigned int FBO_ = 0, RBO_ = 0, framebufferTEX_ = 0;
 
     const float SCREEN_RESOLUTION[2] = { 1024.0f, 720.0f };
 
@@ -42,46 +45,81 @@ void HelloTriangle::Init()
 {
     glEnable(GL_DEPTH_TEST);
 
+    // Objects in the scene.
     crate_ = Model("../data/models/crate/", "crate", "../data/shaders/hello_model/", "model");
+    framebufferShader_ = Shader("../data/shaders/hello_framebuffer/framebuffer.vert", "../data/shaders/hello_framebuffer/framebuffer.frag");
 
-    quad_.Init
-    (
-        std::array<glm::vec3, 4> // points
-        {
-            glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec3(1.0f, 0.0f, 0.0f),
-            glm::vec3(1.0f, 1.0f, 0.0f),
-            glm::vec3(0.0f, 1.0f, 0.0f)
-        },
-        glm::vec3(-2.0f, 0.0f, 0.0f), // position
-        glm::vec3(glm::radians(45.0f), glm::radians(45.0f), 0.0f), // rotation
-        "plain", // shader to use
-        [this](Shader shader)->void // shader's on init
-        {
-            shader.SetMat4("projection", glm::perspective(glm::radians(45.0f), SCREEN_RESOLUTION[0] / SCREEN_RESOLUTION[1], 0.1f, 100.0f));
-            shader.SetVec3("color", glm::vec3(1.0f, 1.0f, 1.0f));
-        },
-        [this](Shader shader)->void // shader's on draw
-        {
-            shader.SetMat4("view", camera_.GetViewMatrix());
-            shader.SetMat4("model", quad_.GetTransform3D().model);
-        }
-    );
+    // Quad's data.
+    float quad[24] =
+    {
+        -1.0f, -1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, 1.0f, 1.0f,
+
+        -1.0f, -1.0f, 0.0f, 0.0f,
+         1.0f,  1.0f, 1.0f, 1.0f,
+        -1.0f,  1.0f, 0.0f, 1.0f
+    };
+    glGenVertexArrays(1, &quadVAO_);
+    glBindVertexArray(quadVAO_);
+    glGenBuffers(1, &quadVBO_);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO_);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)(sizeof(float) * 2));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Framebuffer.
+    glGenFramebuffers(1, &FBO_);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO_);
+
+    glGenTextures(1, &framebufferTEX_);
+    glBindTexture(GL_TEXTURE_2D, framebufferTEX_);
+    // Note that we're using the texture the size of our screen, although it is not mandatory.
+    // TODO: Try to make the rendering happen on a smaller framebuffer resolution than the screen's for a pixelated effect.
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (GLint)SCREEN_RESOLUTION[0], (GLint)SCREEN_RESOLUTION[1], 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTEX_, 0);
+
+    glGenRenderbuffers(1, &RBO_);
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO_);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, (GLint)SCREEN_RESOLUTION[0], (GLint)SCREEN_RESOLUTION[1]);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO_);
+
+    assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void HelloTriangle::Update(seconds dt)
 {
+    // NOTE: If rendering to a smaller renderbuffer than your screen, set glViewport to the texture's size!
+    // Render our scene as usual but onto our framebuffer instead of the default one.
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO_);
     glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    glEnable(GL_DEPTH_TEST); // We want to depth test for the regular scene.
     crate_.Draw(camera_);
-    quad_.Draw();
+
+    // Now draw our quad with the texture of the rendered scene on it onto the default framebuffer.
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClearColor(0.6f, 0.6f, 0.6f, 1.0f); // Just to differenciate between our framebuffer's background and the default framebuffer's background.
+    glClear(GL_COLOR_BUFFER_BIT); // We will only be writing color data.
+    glDisable(GL_DEPTH_TEST);
+    framebufferShader_.Bind();
+    glBindVertexArray(quadVAO_);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO_);
+    glBindTexture(GL_TEXTURE_2D, framebufferTEX_);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 void HelloTriangle::Destroy()
 {
     crate_.Destroy();
-    quad_.Destroy();
+    glDeleteFramebuffers(1, &FBO_);
 }
 
 void HelloTriangle::OnEvent(SDL_Event& event)
